45b3f8afce916527a1770b5fc349e0576a003b94
Erik Olson <erikamerico1991@gmail.com>
2023-08-24 22:38:01 -0400

    removed unncessry use state import

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import './App.css';
-import { useState } from 'react';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 import { Outlet } from 'react-router-dom';
