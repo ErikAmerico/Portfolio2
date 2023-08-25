c69f626da66f7ecdc2e744dfabddc171324a088b
Erik Olson <erikamerico1991@gmail.com>
2023-08-25 16:45:33 -0400

    Created general feel and layout of About Me page

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,12 +1,26 @@
 
 
-import './About.css'
+import './About.css';
 
 export default function About() {
-    return (
-        <div>
-            <h1 id='aboutMeTitle'>About Me</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.</p>
-        </div>
-    )
+  return (
+    <div className='aboutContainer'>
+      <img src='images/laptopWave.png' alt="Avatar waving" className='laptopWaveImage' />
+      <div className='textContainer'>
+        <h1 id='aboutMeTitle'>About Me</h1>
+        <p className='aboutMeP'>Hello! I'm Erik Olson, a passionate individual with a diverse background and a drive for continuous learning. 
+        My journey in the tech world began when I delved into the realm of full stack development. 
+        From crafting elegant front-end user interfaces to architecting robust back-end systems, I've been immersing myself in the world of coding to build digital experiences that bring ideas to life.
+        While my professional journey has had me working as a truck driver, I've been using my off-road hours to explore the expansive landscape of software development. 
+        This dual experience has given me a unique perspective that combines a strong work ethic, attention to detail, and an unwavering commitment to problem-solving. 
+        In the world of coding, just like on the road, every challenge presents an opportunity to adapt, learn, and grow.
+        In addition to my passion for coding, I'm also a proud father to a vibrant 1 year old daughter. 
+        Balancing the exciting world of technology with the joy of parenthood has taught me the importance of versatility, time management, and embracing new experiences. 
+        When I'm not immersed in lines of code, you can often find me spending quality time with my family, cherishing the moments that bring us closer together.
+        As I continue to navigate the roads of life, both as a developer and a family man, my goal remains constant: 
+        to embrace the journey, face challenges head-on, and inspire others to embark on their own paths of growth and exploration. 
+        Join me as we navigate the ever-evolving landscape of technology, innovation, and the beautiful journey of personal development.</p>
+      </div>
+    </div>
+  );
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,24 @@
 
-
+import { useState } from 'react';
 import Navigation from '../NavFolder/Navigation.jsx';
 import './Head.css';
 
+const initialName = 'Erik Olson';
+const reversedName = 'Kire Noslo';
+
 // eslint-disable-next-line react/prop-types
 export default function Header() {
+  const [name, setName] = useState(initialName);
+  
+
+  const handleClick = () => {
+    setName(name === initialName ? reversedName : initialName);
+  };
+
+
   return (
     <header >
-      <h1 className='header-title'>Erik Olson</h1>
+      <h1 className='header-title' onClick={handleClick}>{name}</h1>
       <Navigation />
     </header>
   )
