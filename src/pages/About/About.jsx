2fef75226b31c4ce81359db9ba324c9dbbe66419
Erik Olson <erikamerico1991@gmail.com>
2023-08-27 01:51:03 -0400

    headed margin to seperate footer from content on small screens. moved carousel dots down to not interfere with text

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -27,30 +27,30 @@ export default function About() {
                 </div>
                     <div className="content" id='mobileTextContainer'>
                         <h1 id='aboutMeTitle'>About Me</h1>
-                        <Carousel showStatus={false} showThumbs={false} showArrows={false}>
+                        <Carousel showStatus={false} showThumbs={false} showArrows={false} >
                         <div>
-                            <p>
+                            <p className='mobileP'>
                             Hello! I&apos;m Erik Olson, a passionate individual with a diverse background and a drive for continuous learning. 
                              My journey in the tech world began when I delved into the realm of full stack development. 
                             From crafting elegant front-end user interfaces to architecting robust back-end systems, I&apos;ve been immersing myself in the world of coding to build digital experiences that bring ideas to life.
                             </p>
                         </div>
                         <div>
-                            <p>
+                            <p className='mobileP'>
                             While my professional journey has had me working as a truck driver, I&apos;ve been using my off-road hours to explore the expansive landscape of software development. 
                             This dual experience has given me a unique perspective that combines a strong work ethic, attention to detail, and an unwavering commitment to problem-solving. 
                             In the world of coding, just like on the road, every challenge presents an opportunity to adapt, learn, and grow.
                             </p>
                         </div>
                         <div>
-                            <p>
+                            <p className='mobileP'>
                             In addition to my passion for coding, I&apos;m also a proud father to a vibrant 1 year old daughter. 
                             Balancing the exciting world of technology with the joy of parenthood has taught me the importance of versatility, time management, and embracing new experiences. 
                             When I&apos;m not immersed in lines of code, you can often find me spending quality time with my family, cherishing the moments that bring us closer together.
                             </p>
                         </div>
                         <div>
-                            <p>
+                            <p className='mobileP'>
                             As I continue to navigate the roads of life, both as a developer and a family man, my goal remains constant: 
                             to embrace the journey, face challenges head-on, and inspire others to embark on their own paths of growth and exploration. 
                             Join me as we navigate the ever-evolving landscape of technology, innovation, and the beautiful journey of personal development.
