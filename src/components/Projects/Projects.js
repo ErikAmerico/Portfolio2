46cc6ee23ac5fefd3ee4df7e3e437fe21bdc9f1f
Erik Olson <erikamerico1991@gmail.com>
2023-10-30 23:10:38 -0400

    updated resume, resume viewing button, and boxmaze image path

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,66 +1,50 @@
-
 const Projects = [
-    {
-        Title: "Roll Through The Ages",
-        Description:
-          "An online adaption of the popular board game Roll Through The Ages.",
-        Image:
-          "/images/rollThroughTheAges.png",
-        Site: "https://protected-reaches-31766-0c143902740e.herokuapp.com/",
-        GitHub: "https://github.com/corybenton/roll_through_the_ages",
-      },
-      {
-        Title: "Budget Planner",
-        Description:
-          "A website that allows you to keep track of your financials, view spending habits and plan for the future.",
-        Image:
-          "/images/budgetPlanner.png",
-        Site: "https://caffeinatedjitterbug.github.io/BudgetPlanner/",
-        GitHub: "https://github.com/CaffeinatedJitterBug/BudgetPlanner",
-      },
-      {
-        Title: "Weather Application",
-        Description:
-          "Enter a city to see the weather forecast for the upcoming week!",
-        Image:
-          "/images/weatherApp.png",
-        Site: "https://erikamerico.github.io/Weather-Dashboard/",
-        GitHub: "https://github.com/ErikAmerico/Weather-Dashboard",
-      },
-      {
-        Title: "Tech Blog",
-        Description:
-          "A blog website to discuss all things tech.",
-        Image:
-          "/images/techBlog.png",
-        Site: "https://modelviewcontrollerblog-2239baa9f97c.herokuapp.com/",
-        GitHub: "https://github.com/ErikAmerico/MVC-TECH-BLOG",
-      },
-      {
-        Title: "Box Maze",
-        Description:
-          "A simple game of connecting opposite corners of a box with matching colors.",
-        Image:
-          "/images/boxmaze.png",
-        Site: "https://boxmaze.netlify.app/",
-        GitHub: "https://github.com/ErikAmerico/boxMaze",
-      },
-      {
-        Title: "Text Editor",
-        Description:
-          "Save and edit text files in your browser even when offline.",
-        Image:
-          "/images/Jate.png",
-        Site: "https://olsonpwatexteditor-408ff5bc593e.herokuapp.com/",
-        GitHub: "https://github.com/ErikAmerico/PWA-text-editor",
-      },
-]
-
-
-
-
-
-
-
+  {
+    Title: "Roll Through The Ages",
+    Description:
+      "An online adaption of the popular board game Roll Through The Ages.",
+    Image: "/images/rollThroughTheAges.png",
+    Site: "https://protected-reaches-31766-0c143902740e.herokuapp.com/",
+    GitHub: "https://github.com/corybenton/roll_through_the_ages",
+  },
+  {
+    Title: "Budget Planner",
+    Description:
+      "A website that allows you to keep track of your financials, view spending habits and plan for the future.",
+    Image: "/images/budgetPlanner.png",
+    Site: "https://caffeinatedjitterbug.github.io/BudgetPlanner/",
+    GitHub: "https://github.com/CaffeinatedJitterBug/BudgetPlanner",
+  },
+  {
+    Title: "Weather Application",
+    Description:
+      "Enter a city to see the weather forecast for the upcoming week!",
+    Image: "/images/weatherApp.png",
+    Site: "https://erikamerico.github.io/Weather-Dashboard/",
+    GitHub: "https://github.com/ErikAmerico/Weather-Dashboard",
+  },
+  {
+    Title: "Tech Blog",
+    Description: "A blog website to discuss all things tech.",
+    Image: "/images/techBlog.png",
+    Site: "https://modelviewcontrollerblog-2239baa9f97c.herokuapp.com/",
+    GitHub: "https://github.com/ErikAmerico/MVC-TECH-BLOG",
+  },
+  {
+    Title: "Box Maze",
+    Description:
+      "A simple game of connecting opposite corners of a box with matching colors.",
+    Image: "/images/boxMaze.png",
+    Site: "https://boxmaze.netlify.app/",
+    GitHub: "https://github.com/ErikAmerico/boxMaze",
+  },
+  {
+    Title: "Text Editor",
+    Description: "Save and edit text files in your browser even when offline.",
+    Image: "/images/Jate.png",
+    Site: "https://olsonpwatexteditor-408ff5bc593e.herokuapp.com/",
+    GitHub: "https://github.com/ErikAmerico/PWA-text-editor",
+  },
+];
 
 export default Projects;
--- src/pages/Resume/Resume.jsx
+++ src/pages/Resume/Resume.jsx
@@ -1,41 +1,60 @@
-
-import './Resume.css'
-
+import "./Resume.css";
+import { Link } from "react-router-dom";
 
 const skillsData = [
-    { category: 'Front-End', skills: ['HTML', 'CSS', 'Bootstrap', 'React', 'JavaScript'] },
-    { category: 'Back-End', skills: ['Node.js', 'Express'] },
-    { category: 'Database', skills: ['MySQL', 'Sequelize', 'MongoDB', 'Mongoose', 'GraphQL'] },
+  {
+    category: "Front-End",
+    skills: ["HTML", "CSS", "Bootstrap", "React", "JavaScript"],
+  },
+  { category: "Back-End", skills: ["Node.js", "Express"] },
+  {
+    category: "Database",
+    skills: ["MySQL", "Sequelize", "MongoDB", "Mongoose", "GraphQL"],
+  },
 ];
 
-
-
 export default function Resume() {
-    return (
-        <>
-        <h1 id="resumeTitle">Resume</h1>
-        <div>
-        <a href="/myResume.pdf" download="myResume.pdf" id='downloadResumeButton'>Download Resume</a>
+  return (
+    <>
+      <h1 id="resumeTitle">Resume</h1>
+      <div>
+        {/* <a
+          href="/myResume.pdf"
+          //   target="_blank"
+          //   rel="noreferrer"
+          //   to="https://drive.google.com/file/d/1W9Sc7UhQglCVCld5wwjnGA5m6VzSJ6p3/view?usp=drive_link"
+          download="myResume.pdf"
+          id="downloadResumeButton"
+        >
+          Open Resume
+        </a> */}
+        <Link
+          to="https://drive.google.com/file/d/1W9Sc7UhQglCVCld5wwjnGA5m6VzSJ6p3/view?usp=drive_link"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <button id="downloadResumeButton">Open Resume</button>
+        </Link>
         <div className="skills-table">
-                <h2>Proficiencies</h2>
-                <table>
-                    <thead>
-                        <tr>
-                            <th>Category</th>
-                            <th>Skills</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {skillsData.map((category, index) => (
-                            <tr key={index}>
-                                <td>{category.category}</td>
-                                <td>{category.skills.join(', ')}</td>
-                            </tr>
-                        ))}
-                    </tbody>
-                </table>
-            </div>
+          <h2>Proficiencies</h2>
+          <table>
+            <thead>
+              <tr>
+                <th>Category</th>
+                <th>Skills</th>
+              </tr>
+            </thead>
+            <tbody>
+              {skillsData.map((category, index) => (
+                <tr key={index}>
+                  <td>{category.category}</td>
+                  <td>{category.skills.join(", ")}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
         </div>
-        </>
-    )
+      </div>
+    </>
+  );
 }
